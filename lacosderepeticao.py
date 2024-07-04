a84878a8c8616f87d1cc29b97eac8a471d84724f
Yan <158524554+yanro-ds@users.noreply.github.com>
2024-07-03 21:40:55 -0300

    Add files via upload

---
+++ lacosderepeticao.py
@@ -0,0 +1,14 @@
+#x = 0
+#while x < 5:
+#    nome = input('Qual é o seu nome?: ')
+#    x=x+1
+
+x = 0
+animais = []
+
+while x < 5:
+    animal = input('Digite nomes de animais que começam com a letra do seu nome?: ')
+    animais.append(animal)
+    x=x+1
+
+print(animais)
