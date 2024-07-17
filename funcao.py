827337428de95a820f6ce62e392f9e9e3da91141
Yan <158524554+yanro-ds@users.noreply.github.com>
2024-07-16 21:17:04 -0300

    Add files via upload

---
+++ funcao.py
@@ -0,0 +1,40 @@
+#def mensagem():
+#    print('Python')
+#mensagem()
+
+#def mensagem():
+#    print('Lembrete, caro cliente não equeça de pagar')
+
+#def mensagem2(nome):
+#    print('Lembrete, caro cliente não equeça de pagar' +nome+ '!')
+#mensagem2(' Yan')
+
+def mulher():
+    print('Alta')
+    print('Baixa')
+    print('Cabelo longo')
+    print('Cabelo curto')
+    print('Magra')
+    print('Gorda')
+    
+    
+    
+    
+    m1 = str(input('Você é alta/baixa?: '))
+    m2 = str(input('Você tem cabelo longo/curto?: '))
+
+
+
+    if m1 == 'alta':
+        print('Você é alta')
+    else:
+        print('Você é baixa')
+
+    if m2 == 'cabelo longo':
+        print('E tem cabelos longos')
+    else:
+        print('E tem cabelos curtos')
+
+
+        
+mulher()
